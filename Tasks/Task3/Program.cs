1b8b33fdca64397caa104e4fc7fa518c169803b4
TQ1A <jumbodragon737@gmail.com>
2022-07-01 21:57:29 +0500

    Task1

---
+++ Tasks/Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main()
+        {
+            ProDocumentWorker pdocw = new ProDocumentWorker();
+            ExpertDocumentWorker edocw = new ExpertDocumentWorker();
+            DocumentWorker docw = new DocumentWorker();
+            switch (CodeListener())
+            {
+                case "Pro-123":
+                    Console.WriteLine("\nЗагрузка Professional версии...\n" + new String('=', 50));
+                    docw.OpenDocument();
+                    pdocw.EditDocument();
+                    pdocw.SaveDocument();
+                    break;
+
+                case "Exp-123":
+                    Console.WriteLine("\nЗагрузка Expert версии...\n" + new String('=', 50));
+                    docw.OpenDocument();
+                    pdocw.EditDocument();
+                    edocw.SaveDocument();
+                    break;
+
+                default: 
+                    Console.WriteLine("\nЗагрузка Trial версии...\n" + new String('=',50));
+                    docw.OpenDocument();
+                    docw.EditDocument();
+                    docw.SaveDocument();
+                    break;
+            }
+        }
+        static string CodeListener()
+        {
+            Console.WriteLine("Введите ключ продукта");
+            return Console.ReadLine();
+        }
+    }
+}
