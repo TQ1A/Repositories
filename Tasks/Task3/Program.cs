21f3d796b9428db0b1984e6927644016d787b05b
TQ1A <jumbodragon737@gmail.com>
2022-07-04 01:10:44 +0500

    Task1.1

--- Tasks/Task3/Program.cs
+++ Tasks/Task3/Program.cs
@@ -11,34 +11,31 @@ namespace Task3
         static void Main()
         {
             // TODO: CR: Маркин Евгений: не нужно создавать лишние экземпляры классов, тем более если ты ими не пользуешься
-            ProDocumentWorker pdocw = new ProDocumentWorker();
-            ExpertDocumentWorker edocw = new ExpertDocumentWorker();
-            DocumentWorker docw = new DocumentWorker();
             switch (CodeListener())
             {
                 case "Pro-123": 
                     Console.WriteLine("\nЗагрузка Professional версии...\n" + new String('=', 50));
                     // TODO: CR: Маркин Евгений: тут должно быть только создание типов ProDocumentWorker, ExpertDocumentWorker или DocumentWorker
                     // вызов методов нужно осуществлять вне конструкции switch 
-                    docw.OpenDocument();
-                    pdocw.EditDocument();
-                    pdocw.SaveDocument();
+                    DocLoader(new ProDocumentWorker());
                     break;
 
                 case "Exp-123":
                     Console.WriteLine("\nЗагрузка Expert версии...\n" + new String('=', 50));
-                    docw.OpenDocument();
-                    pdocw.EditDocument();
-                    edocw.SaveDocument();
+                    DocLoader(new ExpertDocumentWorker());
                     break;
 
                 default: 
                     Console.WriteLine("\nЗагрузка Trial версии...\n" + new String('=',50));
-                    docw.OpenDocument();
-                    docw.EditDocument();
-                    docw.SaveDocument();
+                    DocLoader(new DocumentWorker());
                     break;
             }
+        }
+        static void DocLoader(DocumentWorker docw)
+        {
+            docw.OpenDocument();
+            docw.EditDocument();
+            docw.SaveDocument();
         }
         static string CodeListener()
         {
