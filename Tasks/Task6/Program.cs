6f3109a0f7842c6fcbc0ad06c6afab57f952d06e
TQ1A <jumbodragon737@gmail.com>
2022-07-27 22:10:55 +0500

    task

---
+++ Tasks/Task6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task6
+{
+    //    Вопросы для самоконтроля:
+    //•	Что такое статический класс? 
+    //•	Что такое вложенный класс? 
+    //•	Что такое расширяющие методы? 
+    //•	Что такое техника делегирования?
+    //Практическое задание:
+    //•	Задание
+    //Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Требуется: создать расширяющий метод для целочисленного массива, который сортирует элементы массива по возрастанию.
+    
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = new int[] { 10, 2, 6, 3, 4, 7, 8, 9, 1, 5 };
+
+            string result = string.Join(" ",Array.ConvertAll(Sorter.IntSorter(numbers), x => x.ToString()));
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Tasks/Task6/Sorter.cs
@@ -0,0 +1,12 @@
+﻿namespace Task6
+{
+    internal static class Sorter
+    {
+        public static int[] IntSorter(this int[] numbers)
+        {
+            Array.Sort(numbers);
+
+            return numbers;
+        }
+    }
+}
