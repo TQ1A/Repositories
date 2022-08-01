f05cc35e8f503234bde346bfaaa1cfb2e98451b4
TQ1A <jumbodragon737@gmail.com>
2022-08-01 20:18:10 +0500

    task

---
+++ Tasks/Task9/DelegatesArray.cs
@@ -0,0 +1,24 @@
+﻿namespace Task9
+{
+    internal class DelegatesArray
+    {
+        public delegate int RandomNumber();
+        public RandomNumber[] DSRN = new RandomNumber[5];
+
+        public DelegatesArray()
+        {
+            for (int i = 0; i < DSRN.Length; i++)
+            {
+                DSRN[i] = new RandomNumber(SelectRandomNumber);
+            }
+        }
+
+        private static int SelectRandomNumber()
+        {
+            Random rnd = new Random((int)System.DateTime.Now.Ticks);
+            int value = rnd.Next(1, 100);
+            Console.WriteLine(value);
+            return value;
+        }
+    }
+}
---
+++ Tasks/Task9/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task9
+{
+    //    Вопросы для самоконтроля:
+    //•	Что такое делегат? 
+    //•	Что такое комбинированный делегат? 
+    //•	Что такое анонимный метод? 
+    //•	Какие есть правила при вызове методов сообщенных с делегатами? 
+    //•	Что такое лямбда выражение? 
+    //•	Чем лямбда выражение отличается от лямбда оператора?
+    //Практическое задание:
+    //•	Задание
+    //Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Создайте анонимный метод, который принимает в качестве аргумента массив делегатов и
+    //возвращает среднее арифметическое возвращаемых значений методов, сообщенных с делегатами в массиве.
+    //Методы, сообщенные с делегатами из массива, возвращают случайное значение типа int. 
+
+    internal class Program
+    {
+        delegate int MidValue(DelegatesArray.RandomNumber[] random);
+        static DelegatesArray _delegatesArray = new DelegatesArray();
+
+        static void Main()
+        {
+            Console.WriteLine(midValue(_delegatesArray.DSRN));
+        }
+
+        static MidValue midValue = delegate (DelegatesArray.RandomNumber[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i]();
+            }
+            int result = sum / arr.Length;
+            return result;
+        };
+    }
+}
