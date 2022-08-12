f1e8152f019dc1322cf6c701abc580091aa41664
TQ1A <jumbodragon737@gmail.com>
2022-08-12 22:53:03 +0500

    task

---
+++ Tasks/Task11/CarCollection.cs
@@ -0,0 +1,33 @@
+﻿namespace Task11
+{
+    internal class CarCollection<T,R>
+    {
+        public int Count
+        {
+            get { return CarNames.Count(); }
+        }
+
+        List<T> CarYears = new List<T>();
+        List<R> CarNames = new List<R>();
+
+        public void Add(T year, R name)
+        {
+            CarYears.Add(year);
+            CarNames.Add(name);
+        }
+
+        public void Clear()
+        {
+            CarYears.Clear();
+            CarNames.Clear();
+        }
+
+        public (T,R) this[int index]
+        {
+            get
+            {
+                return (CarYears[index], CarNames[index]);
+            }
+        }
+    }
+}
---
+++ Tasks/Task11/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task11
+{
+    //    Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Создайте класс CarCollection<T>. Реализуйте в простейшем приближении возможность
+    //использования его экземпляра для создания парка машин.
+    //Минимально требуемый интерфейс взаимодействия с экземпляром, должен включать метод добавления машин
+    //с названием машины и года ее выпуска, индексатор для получения значения элемента по указанному индексу и
+    //свойство только для чтения для получения общего количества элементов. 
+    //Создайте метод удаления всех машин автопарка. 
+
+    internal class Program
+    {
+        static void Main()
+        {
+            CarCollection<int,string> Cars = new CarCollection<int,string>();
+
+            Cars.Add(1999, "car1");
+            Cars.Add(1997, "car2");
+            Cars.Add(2005, "car3");
+
+            Console.WriteLine(Cars.Count);
+
+            Console.Write(@$"Введите индекс автомобиля(1-{Cars.Count}): ");
+            int index = Int32.Parse(Console.ReadLine())-1;
+
+            Console.WriteLine(Cars[index]);
+
+            Cars.Clear();
+
+            Console.WriteLine(Cars.Count);
+        }
+    }
+}
