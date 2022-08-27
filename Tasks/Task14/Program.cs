d56edccf721729f4f3ce3890a70ff85265c7479b
Andrew <jumbodragon737@gmail.com>
2022-08-27 14:35:03 +0500

    task

---
+++ Tasks/Task14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Task14
+{
+    //    Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Создайте метод, который в качестве аргумента принимает массив целых чисел
+    //и возвращает коллекцию всех четных чисел массива.Для формирования коллекции используйте оператор yield. 
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            foreach(int item in Checker(array))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static IEnumerable Checker(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    yield return array[i];
+                }
+            }
+        }
+    }
+}
